9c70ed53b6b813429bc7df08fb9108601406ce78
hansika8-cloud <hansikasaini918@gmail.com>
2026-02-27 23:15:22 +0530

    Added program to display even and odd numbers in range

---
+++ even_odd_range.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int start, end;
+
+    printf("Enter starting number: ");
+    scanf("%d", &start);
+
+    printf("Enter ending number: ");
+    scanf("%d", &end);
+
+    printf("\nEven numbers:\n");
+    for(int i = start; i <= end; i++) {
+        if(i % 2 == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n\nOdd numbers:\n");
+    for(int i = start; i <= end; i++) {
+        if(i % 2 != 0) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
